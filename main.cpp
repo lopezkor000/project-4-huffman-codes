5c8e57bcec84cf506fbbb918540537feaa28ddfd
Kory <74568018+lopezkor000@users.noreply.github.com>
2024-05-02 05:12:05 +0000

    fixed gather and sort

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ struct character {
 
 class myComparator { 
 public: 
-    int operator() (const character& p1, const character& p2) { 
+    int operator() (const character p1, const character p2) { 
         return p1.freq > p2.freq; 
     } 
 };
@@ -26,24 +26,51 @@ void gatherFrequencies() {
     ifstream data("message.txt");
 
     char c;
+    character temp;
     while(data >> c) {
-        if (freqCounts.find(c) != freqCounts.end())
+        if (freqCounts.find(c) != freqCounts.end()){
             freqCounts[c].freq++;
+        }
         else {
-            character temp = {c, 1, NULL, NULL};
-            freqCounts[c] = temp;
+            temp = {c, 1, NULL, NULL};
+            freqCounts.insert({c, temp});
         }
     }
 }
 
 void sortFrequencies() {
-    for (auto key : freqCounts)
+    for (auto key : freqCounts) {
         L.push(key.second);
+        cout << key.first << " | " << key.second.freq << endl;
+    }
 }
+
+// void combineNodes() {
+//     character* Nleft;
+//     character* Nright;
+
+//     Nleft = L.top();
+//     L.pop();
+//     Nright = L.top();
+//     L.pop();
+
+//     int newFreq = Nleft->freq + Nright->freq;
+//     cout << Nleft->freq << " | " << Nright->freq << endl;
+//     character N = {'~', newFreq, Nleft, Nright};
+//     character* Nptr = &N;
+//     L.push(Nptr);
+
+//     cout << N.element << " | " << N.freq << " | " << N.lptr->element << " | " << N.rptr->element << endl;
+// }
+
+// void makeHuffmanTree() {
+
+// }
 
 int main() {
     gatherFrequencies();
     sortFrequencies();
+    // combineNodes();
 
     return 1;
 }
