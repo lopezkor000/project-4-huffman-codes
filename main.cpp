f2f44a99ef7d6831c6522c9a775003fe22c55ec6
Kory <74568018+lopezkor000@users.noreply.github.com>
2024-05-04 22:54:35 +0000

    core dump

--- main.cpp
+++ main.cpp
@@ -15,13 +15,13 @@ struct character {
 
 class myComparator { 
 public: 
-    int operator() (const character p1, const character p2) { 
-        return p1.freq > p2.freq; 
+    int operator() (const character* p1, const character* p2) { 
+        return p1->freq > p2->freq; 
     } 
 };
 
 map<char, character> freqCounts;
-priority_queue<character, vector<character>, myComparator> L;
+priority_queue<character*, vector<character*>, myComparator> L;
 map<string, char> codes;
 
 void gatherFrequencies() {
@@ -42,22 +42,32 @@ void gatherFrequencies() {
 
 void sortFrequencies() {
     for (auto key : freqCounts) {
-        L.push(key.second);
+        L.push(&key.second);
     }
 }
 
 void combineNodes() {
-    character Nleft;
-    character Nright;
-
-    Nleft = L.top();
+    character *Nleft = L.top();
     L.pop();
-    Nright = L.top();
+    character *Nright = L.top();
     L.pop();
 
-    int newFreq = Nleft.freq + Nright.freq;
-    character N = {'~', newFreq, &Nleft, &Nright};
-    L.push(N);
+    // Nleft = L.top();
+    // L.pop();
+    // Nright = L.top();
+    // L.pop();
+
+    int newFreq = Nleft->freq + Nright->freq;
+    character N = {'~', newFreq, Nleft, Nright};
+    L.push(&N);
+
+    // cout << "node: " << N.element;
+    // cout << "\t| ";
+    // cout << "freq: " << N.freq;
+    // cout << "\t| ";
+    // cout << "lptr: " << N.lptr->element;
+    // cout << "\t| ";
+    // cout << "rptr: " << N.rptr->element << endl;
 }
 
 void makeHuffmanTree() {
@@ -66,19 +76,30 @@ void makeHuffmanTree() {
     }
 }
 
-void traverseHuffmanTree(const character* node, string binCode = "") {
-    cout << "Node: " << node->element << endl;
-    if(node->lptr == NULL && node->rptr == NULL) {
-        cout << "Code: " << binCode << endl;
+void const traverseHuffmanTree(const character* node, string binCode = "") {
+    cout << "Node: " << node->element << " | freq: " << node->freq << " | lptr: " << node->lptr->element << " | rptr: " << node->rptr->element << endl;
+    if(node->element != '~') {
+        cout << "Node: " << node->element << " | " << "Code: " << binCode << endl;
         codes.insert({binCode, node->element});
     }
-    cout << "---checking left---" << endl;
-    if(node->lptr != NULL) {
-        traverseHuffmanTree(node->lptr, binCode+"0");
+    else {
+        cout << "---checking left---" << endl;
+        if(&node->lptr != NULL) {
+            traverseHuffmanTree(node->lptr, binCode+"0");
+        }
+        cout << "---checking right---" << endl;
+        if(&node->rptr != NULL) {
+            traverseHuffmanTree(node->lptr, binCode+"1");
+        }
+        cout << "---returning up---" << endl;
     }
-    cout << "---checking right---" << endl;
-    if(node->rptr != NULL) {
-        traverseHuffmanTree(node->lptr, binCode+"1");
+}
+
+void testTree() {
+    const character* node = L.top();
+    while(node->lptr != NULL) {
+        cout << "Node: " << node->element << " | freq: " << node->freq << " | lptr: " << node->lptr->element << " | rptr: " << node->rptr->element << endl;
+        node = node->lptr;
     }
 }
 
@@ -86,8 +107,9 @@ int main() {
     gatherFrequencies();
     sortFrequencies();
     makeHuffmanTree();
-    const character root = L.top();
-    traverseHuffmanTree(&root);
+    const character* root = L.top();
+    // testTree();
+    traverseHuffmanTree(root);
     for (auto key : codes) {
         cout << key.first << "\t" << key.second << endl;
     }
